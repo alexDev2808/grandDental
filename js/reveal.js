2bfac7f510bbdc0d1302d47dfdbf1727c00056e1
Alexis Hernandez <colores.2808.8000@gmail.com>
2022-03-24 18:44:28 -0600

    Se agrego la libreria scrollReveal.js para dar efectos al hacer scroll en ciertas secciones de la pagina

---
+++ js/reveal.js
@@ -0,0 +1,67 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.inicio__titulos', {
+        duration: 1600,
+        origin: 'left',
+        distance: '-100px',
+        easing: 'ease-out',
+    });
+    sr.reveal('.botones__contacto', {
+        duration: 1600,
+        origin: 'top',
+        distance: '-100px',
+        easing: 'ease-out',
+    });
+    sr.reveal('.especialidades__textos', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    });
+    sr.reveal('.cuadricula-especialidades', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px',
+    });
+    sr.reveal('.seccion-youtube__textos', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px',
+    });
+    sr.reveal('.seccion-youtube__video', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px',
+    });
+    sr.reveal('.seccion-galeria__textos', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px',
+    });
+    sr.reveal('.seccion-galeria__cuadricula', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px',
+    });
+    sr.reveal('.articuloMision', {
+        duration: 3000,
+        origin: 'top',
+        distance: '100px',
+    });
+    sr.reveal('.articuloVision', {
+        duration: 3600,
+        origin: 'bottom',
+        distance: '100px',
+    });
+    sr.reveal('.nuestrosValores__titulo', {
+        duration: 4000,
+        origin: 'left',
+        distance: '200px',
+    });
+    sr.reveal('.contenedorValores', {
+        duration: 5000,
+        origin: 'bottom',
+        distance: '180px',
+    });
+
+
+    
